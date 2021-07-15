83608c6e490fab54cac67292da26d76ea45d346a
zfy6 <65937097+zfy6@users.noreply.github.com>
2021-07-15 20:28:30 +0800

    Add files via upload

--- BearM/js/common.js
+++ BearM/js/common.js
@@ -1,5 +1,12 @@
-window.onresize = function(){
+window.onload = function () {
+    setFontSize();
+    window.onresize = function () {
+        setFontSize();
+    }
+}
+
+function setFontSize() {
     let width = window.screen.width;
-    let font = width/750*100;
-    document.documentElement.style.fontSize = font +'px';
+    let font = width / 750 * 100;
+    document.documentElement.style.fontSize = font + 'px';
 }
